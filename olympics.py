e798a4caa6e9e209a77d8c3d7f1fcc83a95d07d7
Jared McKnight <jmcknight@archertech.ca>
2018-02-13 17:13:00 -0500

    adds basic caching, since medal counts aren't changing that often

--- olympics.py
+++ olympics.py
@@ -1,4 +1,5 @@
 
+import datetime
 import logging
 import requests
 from bs4 import BeautifulSoup
@@ -10,15 +11,27 @@ class Olympics(BotPlugin):
     RESULT_TABLE_CLASS = 'ResTableFull'
     MEDALS_URL = 'https://www.pyeongchang2018.com/en/game-time/results/OWG2018/en/general/medal-standings.htm'
 
+    CACHE_LENGTH = 300
+    CACHE_EXPIRES = None
+    CACHED_MEDALS_PAGE = None
+
     def activate(self):
         super(Olympics, self).activate()
+
+
+    def _load_medals_page(self):
+        current_time = datetime.datetime.utcnow().timestamp()
+        if not self.CACHE_EXPIRES or not self.CACHED_MEDALS_PAGE or self.CACHE_EXPIRES <= current_time:
+            response = requests.get(self.MEDALS_URL)
+            self.CACHED_MEDALS_PAGE = response.content
+            self.CACHE_EXPIRES = (current_time + self.CACHE_LENGTH)
 
 
     @botcmd
     def medals(self, msg, args):
 
-        response = requests.get(self.MEDALS_URL)
-        soup = BeautifulSoup(response.content, 'html.parser')
+        self._load_medals_page()
+        soup = BeautifulSoup(self.CACHED_MEDALS_PAGE, 'html.parser')
         results_table = soup.find('table', attrs={'class': self.RESULT_TABLE_CLASS})
 
         for row in results_table.find_all('tr'):
